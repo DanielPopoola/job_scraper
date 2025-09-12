536571746650d194b13fab70387f5f4aa96d30f7
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-09-12 06:05:35 +0100

    Implemented detailed generic views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from django.db.models import Count, Q, Avg, Max, F
 from django.utils import timezone
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
